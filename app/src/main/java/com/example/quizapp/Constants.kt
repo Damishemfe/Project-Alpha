e7af55a806d1d4eb750f928449d694d499b3de4b
JosevfSid <josefsidhd@gmail.com>
2020-08-05 02:17:23 +0200

    Stage 1

--- app/src/main/java/com/example/quizapp/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.example.quizapp
-
-import java.util.ArrayList
-
-object Constants {
-    fun getQuestions(): ArrayList<Question>{
-        val questionslist = ArrayList<Question>()
-    }
-}
